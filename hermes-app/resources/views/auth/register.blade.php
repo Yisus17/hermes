b1ce9d319df0fafae2a5659fe4fcf6381cd84df3
Jesus Arevalo <jarevalo@moddo.com>
2021-03-07 22:23:58 +0100

    Agregamos el rol en el formulario de registro

--- hermes-app/resources/views/auth/register.blade.php
+++ hermes-app/resources/views/auth/register.blade.php
@@ -39,6 +39,21 @@
                             </div>
                         </div>
 
+
+                        <div class="form-group row">
+                            <label for="type" class="col-md-4 col-form-label text-md-right">{{ __('Type') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="type" type="text" class="form-control @error('type') is-invalid @enderror" name="type" value="{{ old('type') }}" required autocomplete="type">
+
+                                @error('type')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
