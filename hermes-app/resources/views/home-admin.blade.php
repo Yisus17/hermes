bdf06714db825eb476271576da39c3d6d5c8f115
Jesus Arevalo <jarevalo@moddo.com>
2021-03-14 10:57:45 +0100

    Estilos y dashboard

--- hermes-app/resources/views/home-admin.blade.php
+++ hermes-app/resources/views/home-admin.blade.php
@@ -3,21 +3,72 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="col-md-12">
+            <h2>Dashboard</h2>
+            <div class="card-deck mt-4">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                <!-- CLIENTS -->
+                <div class="card">
+                    <a href="{{ url('/users') }}">
+                        <div class="image-dashboard-card cyan">
+                            <i class="fas fa-user-friends"></i>
                         </div>
-                    @endif
 
-                    {{ __('You are logged in admin!') }}
+                        <div class="card-body">
+                            <h5 class="card-title">Usuarios</h5>
+                            <p class="card-text">En esta sección podrás gestionar los datos de tus contactos.</p>
+                        </div>
+                    </a>
                 </div>
+
+                <!-- PRODUCTS -->
+                <div class="card">
+                    <a href="{{ url('/products') }}">
+                        <div class="image-dashboard-card yellow">
+                            <i class="fas fa-headphones-alt"></i>
+                        </div>
+
+                        <div class="card-body">
+                            <h5 class="card-title">Emprendimientos</h5>
+                            <p class="card-text">En esta sección podrás gestionar los productos de tu inventario.</p>
+                        </div>
+                    </a>
+                </div>
+
+                <!-- BUDGETS -->
+                <div class="card">
+                    <a href="{{ url('/budgets') }}">
+                        <div class="image-dashboard-card red">
+                            <i class="fas fa-hand-holding-usd"></i>
+                        </div>
+
+                        <div class="card-body">
+                            <h5 class="card-title">Presupuestos</h5>
+                            <p class="card-text">En esta sección podrás crear, editar y descargar tus presupuestos.</p>
+                        </div>
+                    </a>
+                </div>
+
+                <!-- Invoices -->
+                <div class="card">
+                    <a href="{{ url('/invoices') }}">
+                        <div class="image-dashboard-card green">
+                            <i class="fas fa-file-invoice-dollar"></i>
+                        </div>
+
+                        <div class="card-body">
+                            <h5 class="card-title">Facturas</h5>
+                            <p class="card-text">En esta sección podrás crear, editar y descargar tus facturas.</p>
+                        </div>
+                    </a>
+                </div>
+
+                
+
             </div>
         </div>
+
+        
     </div>
 </div>
 @endsection
